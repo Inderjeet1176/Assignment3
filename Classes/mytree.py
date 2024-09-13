a34ff1bbdb4767c278eb8243003e5af1a0472ade
Inderjeet1176 <150043429+Inderjeet1176@users.noreply.github.com>
2024-09-13 22:42:25 +0200

    [green] Implement adding a value to the tree

--- Classes/mytree.py
+++ Classes/mytree.py
@@ -5,3 +5,13 @@ class BinaryTree:
     def is_empty(self):
         return self.root is None
     
+    class MyTree:
+        def __init__(self):
+          self.root = None
+
+        def is_empty(self):
+            return self.root is None
+
+        def add(self, value):
+            self.root = value
+
