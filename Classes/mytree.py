ec7575f6ebf85834a90fb91676608a422d7d8345
Inderjeet1176 <150043429+Inderjeet1176@users.noreply.github.com>
2024-09-13 22:48:19 +0200

    [green] Implement contains method to check if tree contains a value

--- Classes/mytree.py
+++ Classes/mytree.py
@@ -15,3 +15,17 @@ class BinaryTree:
         def add(self, value):
             self.root = value
 
+
+    class MyTree:
+          def __init__(self):
+             self.root = None
+
+          def is_empty(self):
+             return self.root is None
+
+          def add(self, value):
+             self.root = value
+
+          def contains(self, value):
+              return self.root == value
+
